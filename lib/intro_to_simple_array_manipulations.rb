4dbb7879dfe08255429114b97878c29b5aea6979
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-01-28 23:10:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,5 +3,6 @@ def using_concat(my_favorite_things, more_favs)
 end
 
 def using_insert(list_of_programming_languages, another_language)
+  new_array = []
   list_of_programming_languages[4].insert(another_language)
 end
