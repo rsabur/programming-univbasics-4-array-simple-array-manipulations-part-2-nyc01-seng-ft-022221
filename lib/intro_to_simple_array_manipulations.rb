61a3992077630fa7b678a5270d7e58813772dd2f
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-01-28 23:13:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,5 +3,6 @@ def using_concat(my_favorite_things, more_favs)
 end
 
 def using_insert(list_of_programming_languages, another_language)
+  another_language = "Python"
   list_of_programming_languages[4] = another_language
 end
