5b40abf6c4732eed44b06e0e61ba7bc9a943c072
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-01-28 23:26:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,5 +4,9 @@ end
 
 def using_insert(list_of_programming_languages, another_language)
   list_of_programming_languages.insert(4, another_language)
-  
+
+end
+
+def using_uniq(haircuts)
+  haircuts.uniq
 end
