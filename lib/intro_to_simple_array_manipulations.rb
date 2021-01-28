6d17dfc78146eda3c8cdda7896522a9f5d333c67
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-01-28 23:32:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,6 +19,6 @@ def using_delete(instructors, no_offense_steven)
   instructors.delete(no_offense_steven)
 end
 
-def using_delete_at(famous_robots, 2)
-  famous_robots.delete_at(2)
+def using_delete_at(famous_robots, deleted_robot)
+  famous_robots.delete_at(deleted_robot)
 end
